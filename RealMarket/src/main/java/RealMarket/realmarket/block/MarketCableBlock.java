939b709f790d257427603e838038bee45732d129
Oleg <kolotilkinoleg12@gmail.com>
2026-07-02 19:49:05 +0300

    реалізовано кабель
    додано повністю робочий кабель (реалізовано через систему multipart), також перероблено модель та хітбокси торгового терміналу та трішки змінено display для маркет лінку

--- RealMarket/src/main/java/RealMarket/realmarket/block/MarketCableBlock.java
+++ RealMarket/src/main/java/RealMarket/realmarket/block/MarketCableBlock.java
@@ -1,13 +1,15 @@
 package RealMarket.realmarket.block;
 
+import RealMarket.realmarket.RealMarket;
 import RealMarket.realmarket.blockentity.MarketCableBlockEntity;
-import RealMarket.realmarket.blockentity.MarketLinkBlockEntity;
+import appeng.api.networking.IInWorldGridNodeHost;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DirectionalBlock;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
@@ -19,9 +21,10 @@ import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
+import java.util.Map;
 
-public class MarketCableBlock extends Block implements EntityBlock {
+@SuppressWarnings("deprecation")
+public class MarketCableBlock extends DirectionalBlock implements EntityBlock {
     public static final BooleanProperty NORTH = BlockStateProperties.NORTH;
     public static final BooleanProperty SOUTH = BlockStateProperties.SOUTH;
     public static final BooleanProperty EAST = BlockStateProperties.EAST;
@@ -29,76 +32,76 @@ public class MarketCableBlock extends Block implements EntityBlock {
     public static final BooleanProperty UP = BlockStateProperties.UP;
     public static final BooleanProperty DOWN = BlockStateProperties.DOWN;
 
-    // Частини форми для кожного напрямку
-    private static final VoxelShape CENTER_SHAPE = Block.box(6, 6, 6, 10, 10, 10);
-    private static final VoxelShape UP_SHAPE = Block.box(6, 10, 6, 10, 16, 10);
-    private static final VoxelShape DOWN_SHAPE = Block.box(6, 0, 6, 10, 6, 10);
-    private static final VoxelShape NORTH_SHAPE = Block.box(6, 6, 0, 10, 10, 6);
-    private static final VoxelShape SOUTH_SHAPE = Block.box(6, 6, 10, 10, 10, 16);
-    private static final VoxelShape WEST_SHAPE = Block.box(0, 6, 6, 6, 10, 10);
-    private static final VoxelShape EAST_SHAPE = Block.box(10, 6, 6, 16, 10, 10);
+    private static final Map<Direction, BooleanProperty> PROPS = Map.of(
+            Direction.NORTH, NORTH, Direction.SOUTH, SOUTH,
+            Direction.EAST, EAST, Direction.WEST, WEST,
+            Direction.UP, UP, Direction.DOWN, DOWN
+    );
+
+    private static final VoxelShape[] SHAPES = new VoxelShape[64];
 
     public MarketCableBlock(Properties p) {
         super(p);
-        this.registerDefaultState(this.stateDefinition.any()
+        registerDefaultState(stateDefinition.any()
+                .setValue(FACING, Direction.NORTH)
                 .setValue(NORTH, false).setValue(SOUTH, false).setValue(EAST, false)
                 .setValue(WEST, false).setValue(UP, false).setValue(DOWN, false));
+        runCache();
     }
 
-    private boolean canConnect(BlockGetter world, BlockPos pos, Direction dir) {
-        BlockEntity be = world.getBlockEntity(pos.relative(dir));
-        return be instanceof MarketCableBlockEntity || be instanceof MarketLinkBlockEntity;
+    private void runCache() {
+        VoxelShape core = box(6, 6, 6, 10, 10, 10);
+        VoxelShape[] sides = {
+                box(6, 0, 6, 10, 6, 10), box(6, 10, 6, 10, 16, 10),
+                box(6, 6, 0, 10, 10, 6), box(6, 6, 10, 10, 10, 16),
+                box(0, 6, 6, 6, 10, 10), box(10, 6, 6, 16, 10, 10)
+        };
+        for (int i = 0; i < 64; i++) {
+            VoxelShape s = core;
+            for (int b = 0; b < 6; b++) if ((i & (1 << b)) != 0) s = Shapes.or(s, sides[b]);
+            SHAPES[i] = s;
+        }
+    }
+
+    @Override
+    @Nonnull
+    public VoxelShape getShape(@Nonnull BlockState s, @Nonnull BlockGetter l, @Nonnull BlockPos p, @Nonnull CollisionContext c) {
+        int i = 0;
+        if (s.getValue(DOWN)) i |= 1; if (s.getValue(UP)) i |= 2;
+        if (s.getValue(NORTH)) i |= 4; if (s.getValue(SOUTH)) i |= 8;
+        if (s.getValue(WEST)) i |= 16; if (s.getValue(EAST)) i |= 32;
+        return SHAPES[i];
+    }
+
+    private boolean canConnect(BlockGetter level, BlockPos pos, Direction dir) {
+        BlockPos target = pos.relative(dir);
+        BlockState state = level.getBlockState(target);
+        if (state.is(this) || state.is(RealMarket.TRADE_BLOCK.get()) || state.is(RealMarket.MARKET_LINK_BLOCK.get())) return true;
+        return level.getBlockEntity(target) instanceof IInWorldGridNodeHost;
     }
 
     @Override
     public BlockState getStateForPlacement(BlockPlaceContext ctx) {
-        return makeConnections(ctx.getLevel(), ctx.getClickedPos());
+        BlockState state = defaultBlockState().setValue(FACING, ctx.getNearestLookingDirection().getOpposite());
+        for (Direction d : Direction.values()) {
+            state = state.setValue(PROPS.get(d), canConnect(ctx.getLevel(), ctx.getClickedPos(), d));
+        }
+        return state;
     }
 
-    public BlockState makeConnections(BlockGetter world, BlockPos pos) {
-        return defaultBlockState()
-                .setValue(NORTH, canConnect(world, pos, Direction.NORTH))
-                .setValue(SOUTH, canConnect(world, pos, Direction.SOUTH))
-                .setValue(EAST, canConnect(world, pos, Direction.EAST))
-                .setValue(WEST, canConnect(world, pos, Direction.WEST))
-                .setValue(UP, canConnect(world, pos, Direction.UP))
-                .setValue(DOWN, canConnect(world, pos, Direction.DOWN));
-    }
-
+    @Override
     @Nonnull
-    @Override
-    @SuppressWarnings("deprecation")
-    public BlockState updateShape(@Nonnull BlockState state, @Nonnull Direction dir, @Nonnull BlockState adj,
-                                  @Nonnull LevelAccessor world, @Nonnull BlockPos pos, @Nonnull BlockPos adjPos) {
-        BooleanProperty prop = switch (dir) {
-            case NORTH -> NORTH; case SOUTH -> SOUTH; case EAST -> EAST;
-            case WEST -> WEST; case UP -> UP; case DOWN -> DOWN;
-        };
-        return state.setValue(prop, canConnect(world, pos, dir));
-    }
-
-    @Nonnull
-    @Override
-    @SuppressWarnings("deprecation")
-    public VoxelShape getShape(BlockState state, @Nonnull BlockGetter world, @Nonnull BlockPos pos, @Nonnull CollisionContext context) {
-        VoxelShape shape = CENTER_SHAPE;
-        if (state.getValue(UP)) shape = Shapes.or(shape, UP_SHAPE);
-        if (state.getValue(DOWN)) shape = Shapes.or(shape, DOWN_SHAPE);
-        if (state.getValue(NORTH)) shape = Shapes.or(shape, NORTH_SHAPE);
-        if (state.getValue(SOUTH)) shape = Shapes.or(shape, SOUTH_SHAPE);
-        if (state.getValue(WEST)) shape = Shapes.or(shape, WEST_SHAPE);
-        if (state.getValue(EAST)) shape = Shapes.or(shape, EAST_SHAPE);
-        return shape;
+    public BlockState updateShape(BlockState s, @Nonnull Direction d, @Nonnull BlockState adj, @Nonnull LevelAccessor level, @Nonnull BlockPos p, @Nonnull BlockPos ap) {
+        return s.setValue(PROPS.get(d), canConnect(level, p, d));
     }
 
     @Override
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(NORTH, SOUTH, EAST, WEST, UP, DOWN);
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> b) {
+        b.add(FACING, NORTH, SOUTH, EAST, WEST, UP, DOWN);
     }
 
-    @Nullable
     @Override
-    public BlockEntity newBlockEntity(@Nonnull BlockPos pos, @Nonnull BlockState state) {
-        return new MarketCableBlockEntity(pos, state);
+    public BlockEntity newBlockEntity(@Nonnull BlockPos p, @Nonnull BlockState s) {
+        return new MarketCableBlockEntity(p, s);
     }
 }
